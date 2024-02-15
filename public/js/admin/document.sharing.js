a2265fcc4e391336cd19247ec5982137fe6cb545
Lucas <lucas.sanner@codalia.fr>
2024-02-15 16:54:14 +0000

    Integrates the document view into the iframe of the Bootstrap modal dialog.

--- public/js/admin/document.sharing.js
+++ public/js/admin/document.sharing.js
@@ -3,9 +3,16 @@
     // Run a function when the page is fully loaded including graphics.
     document.addEventListener('DOMContentLoaded', () => {
 
-        document.getElementById('documentSharing').addEventListener('click', function(e) {
-            //e.preventDefault();
-            alert('document');
+        document.getElementById('myModal').addEventListener('focus', function(e) {
+            var iframe = document.getElementById('documentIframe');
+            // Get the total height of the document
+            const totalHeight = document.documentElement.scrollHeight;
+            // and the height of the iframe.
+            const iframeHeight = iframe.contentWindow.document.documentElement.scrollHeight
+            // Make sure the iframe is not higher than the document.
+            const height = iframeHeight < totalHeight ? iframeHeight : totalHeight; 
+            // Set the height of the iframe.
+            iframe.style.height = height + 'px';
         });
     });
 
--- resources/views/admin/partials/document-sharing.blade.php
+++ resources/views/admin/partials/document-sharing.blade.php
@@ -1,6 +1,27 @@
 <div class="col-md-6 form-group">
     <label>{{ __('labels.generic.attestation') }}</label>
     <div>
-        <button type="button"  class="btn btn-primary" id="documentSharing" data-url="{{ route('cms.filemanager.index') }}">Ajouter</button>
+        <button type="button" class="btn btn-primary" id="modalButton" data-bs-toggle="modal" data-bs-target="#myModal">
+          Open modal
+        </button>
     </div>
+</div>
+
+<div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+          <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+              <div class="embed-responsive embed-responsive-16by9">
+                  <iframe class="modal-iframe embed-responsive-item" id="documentIframe" src="{{ route('cms.filemanager.index') }}" frameborder="0"></iframe>
+              </div>
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+  </div>
 </div>
